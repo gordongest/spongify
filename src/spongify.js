98e3df3cca78f541290c2717e932e9344df04364
Gordon Gest <gordon.gest@gmail.com>
2022-01-19 08:27:13 -0700

    refactor to case letters I and L consistently

--- src/spongify.js
+++ src/spongify.js
@@ -3,10 +3,12 @@ exports.__esModule = true;
 exports.spongify = void 0;
 var spongify = function (str, step) {
     if (step === void 0) { step = 4; }
-    var letters = str.toLowerCase().split('');
-    for (var i = 1; i < letters.length - (step - 1); i++) {
+    var letters = str.toLowerCase().replace(/l/ig, 'L').split('');
+    for (var i = 0; i < letters.length - (step - 1); i++) {
         var randomInt = Math.floor(Math.random() * step);
-        letters[i + randomInt] = letters[i + randomInt].toUpperCase();
+        if (letters[i + randomInt] !== 'i') {
+            letters[i + randomInt] = letters[i + randomInt].toUpperCase();
+        }
     }
     return letters.join('');
 };
--- src/spongify.ts
+++ src/spongify.ts
@@ -1,8 +1,13 @@
 export const spongify = (str: string, step: number = 4): string => {
-  const letters = str.toLowerCase().split('');
-  for (let i = 1; i < letters.length - (step - 1); i++) {
+  const letters: string[] = str.toLowerCase().replace(/l/ig, 'L').split('');
+
+  for (let i = 0; i < letters.length - (step - 1); i++) {
       let randomInt = Math.floor(Math.random() * step);
-      letters[i + randomInt] = letters[i + randomInt].toUpperCase();
+
+      if (letters[i + randomInt] !== 'i') {
+        letters[i + randomInt] = letters[i + randomInt].toUpperCase();
+      }
   }
+
   return letters.join('');
 }
